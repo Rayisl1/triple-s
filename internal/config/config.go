6325d549628fabb010469cfdef2d12b5adefe2b8
nualdaber <nurss.aldb@gmail.com>
2026-01-28 14:32:39 +0500

    new config anderrors

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,33 +1,56 @@
-package main
+package config
 
 import (
+	"errors"
 	"flag"
-	"fmt"
-	"os"
 )
 
-func main() {
-	port := flag.Int("port", 8080, "Port number")
-	dir := flag.String("dir", "./data", "Path to the directory")
-	help := flag.Bool("help", false, "Show this screen")
+type Config struct {
+	Port int
+	Dir  string
+	Help bool
+}
 
-	flag.Usage = func() {
-		fmt.Printf("Simple Storage Service.\n\n")
-		fmt.Printf("**Usage:**\n")
-		fmt.Printf("    triple-s [-port <N>] [-dir <S>]\n")
-		fmt.Printf("    triple-s --help\n\n")
-		fmt.Printf("**Options:**\n")
-		fmt.Printf("- --help     Show this screen.\n")
-		fmt.Printf("- --port N   Port number\n")
-		fmt.Printf("- --dir S    Path to the directory\n")
+func UsageText() string {
+	return `Simple Storage Service.
+
+Usage:
+  triple-s [-port <N>] [-dir <S>]
+  triple-s --help
+
+Options:
+  --help     Show this screen.
+  --port N   Port number
+  --dir S    Path to the directory
+`
+}
+
+func Parse(args []string) (Config, error) {
+	cfg := Config{}
+
+	flagSet := flag.NewFlagSet("triple-s", flag.ContinueOnError)
+	flagSet.SetOutput(nil)
+
+	flagSet.IntVar(&cfg.Port, "port", 8080, "Port number")
+	flagSet.StringVar(&cfg.Dir, "dir", "data", "Path to the directory")
+	flagSet.BoolVar(&cfg.Help, "help", false, "Show help")
+
+	err := flagSet.Parse(args)
+	if err != nil {
+		return Config{}, err
 	}
 
-	flag.Parse()
-
-	if *help {
-		flag.Usage()
-		os.Exit(0)
+	if cfg.Help {
+		return cfg, nil
 	}
 
-	fmt.Printf("Starting Triple-S on port %d, serving directory: %s\n", *port, *dir)
+	if cfg.Port < 1 || cfg.Port > 65535 {
+		return Config{}, errors.New("wrong port number")
+	}
+
+	if cfg.Dir == "" {
+		return Config{}, errors.New("directory is empty")
+	}
+
+	return cfg, nil
 }
