f7b37c79cd2fc4e5568eee2ffb102eb34b208fdc
nualdaber <nurss.aldb@gmail.com>
2026-01-28 17:35:44 +0500

    buckets_csv.go -> fs.go

--- internal/storage/buckets_csv.go
+++ internal/storage/buckets_csv.go
@@ -38,20 +38,8 @@ func ListBuckets(baseDir string) ([]BucketMeta, error) {
 
 	return buckets, nil
 }
-func IsBucketEmpty(basedir, bucket string) (bool, error) {
-	dir := filepath.Join(basedir, bucket)
+func DeleteBuckets(baseDir string) ([]BucketMeta, error) {
+	if IsBucketEmpty(baseDir) {
 
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return false, err
 	}
-	for _, e := range entries {
-		if e.Name() != "object.go" {
-			return false, err
-		}
-	}
-	return true, nil
-}
-func RemoveAllFromBuckets(basedir, bucket string) error {
-	return os.RemoveAll(filepath.Join(basedir, bucket))
 }
